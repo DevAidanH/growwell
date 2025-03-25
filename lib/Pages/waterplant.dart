63a6bfad60da862757681887d37e0e4d5f7872b9
DevAidan <aidanhumpidge333@gmail.com>
2025-03-25 20:09:20 +0000

    Basic functions all working.

--- lib/Pages/waterplant.dart
+++ lib/Pages/waterplant.dart
@@ -13,6 +13,21 @@ class Waterplant extends StatefulWidget {
 
 class _WaterplantState extends State<Waterplant> {
   bool isVisible = false;
+  DateTime? selectedDate;
+  DateTime tempDate = DateTime.now();
+
+  Future<void> _selectDate() async {
+      final DateTime? pickedDate = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2030), //Fix date range here
+      );
+
+      setState(() {
+        selectedDate = pickedDate;
+      });
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -44,6 +59,54 @@ class _WaterplantState extends State<Waterplant> {
                     color: Colors.blueAccent,
                     height: 500,
                     width: 300,
+                    child: Column(
+                      children: [
+                        Text("Well Done! You've kept this plant alive again!"),
+                        Text("When will this plant need water again"),
+                        Row(
+                          children: [
+                            MaterialButton(
+                              onPressed: (){
+                                selectedDate = DateTime(tempDate.year, tempDate.month, tempDate.day + 1);
+
+                                setState(() {
+                                  selectedDate = selectedDate;
+                                });
+                              }, 
+                              child: Text("1 Day")
+                            ),
+                            MaterialButton(
+                              onPressed: (){
+                                selectedDate = DateTime(tempDate.year, tempDate.month, tempDate.day + 7);
+
+                                setState(() {
+                                  selectedDate = selectedDate;
+                                });
+                              }, 
+                              child: Text("1 Week")
+                            ),
+                            MaterialButton(
+                              onPressed: (){
+                                selectedDate = DateTime(tempDate.year, tempDate.month + 1, tempDate.day);
+
+                                setState(() {
+                                  selectedDate = selectedDate;
+                                });
+                              }, 
+                              child: Text("1 Month")
+                            ),
+                          ],
+                        ),
+                        Text("Or please select a date"),
+                        Text(
+                          selectedDate != null
+                              ? '${selectedDate!.day}/${selectedDate!.month}/${selectedDate!.year}'
+                              : 'No date selected',
+                        ),
+                        MaterialButton(onPressed: _selectDate, child: const Text('Select Date')),
+                        MaterialButton(onPressed: () => value.updateDate(widget.plantName, selectedDate, context), child: Text("Save"),)
+                      ],
+                    ),
                   ),
             ),
           ],
--- lib/data/plant_data.dart
+++ lib/data/plant_data.dart
@@ -72,29 +72,9 @@ class PlantData extends ChangeNotifier {
     Navigator.push(context, MaterialPageRoute(builder: (context) => Homepage()));
   }
   
-  /*void waterPlant(name, context){
-    DateTime? selectedDate;
-    Future<void> _selectDate() async {
-    final DateTime? pickedDate = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime.now(),
-      lastDate: DateTime(2030), //Fix date range here
-    );
-      setState(() {
-        selectedDate = pickedDate;
-      });
-    }
-
-    showDialog(context: context, builder: (context) => AlertDialog(
-      title: Text("Well Done! You kept this plant alive! Please select when this plant will need water again"),
-      content: Text(selectedDate != null
-                    ? '${selectedDate!.day}/${selectedDate!.month}/${selectedDate!.year}'
-                    : 'No date selected',
-                ),
-      actions: [
-          OutlinedButton(onPressed: _selectDate, child: const Text('Select Date')),
-      ],
-    ));
-  }*/
+  void updateDate(name, date, context){
+    Plant relevantPlant = getRelevantPlant(name);
+    relevantPlant.upcomingDate = convertDateTimeToYYYYMMDD(date);
+    Navigator.push(context, MaterialPageRoute(builder: (context) => Homepage()));
+  }
 }
